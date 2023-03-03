0a566a1fcf3615cf927e88514e1de48abac9f713
Ammar Khan <2019ee34@student.uet.edu.pk>
2023-03-03 15:14:52 +0500

    changed instr_mem and test_bench
    Adding a signature dump logic

--- Simulation/Instruction_Memory.sv
+++ Simulation/Instruction_Memory.sv
@@ -3,11 +3,17 @@ module Instruction_Memory (
     output logic [31:0] Inst
 );
 
+logic [1023:0] kpl;
 logic [31:0] inst_memory [1023:0]; 
 
 initial begin
-    $readmemh("instruction.txt", inst_memory); 
+    if($value$plusargs ( "mem_init=%s",kpl));
+        $readmemh(kpl, inst_memory);
 end
+
+// initial begin
+//     $readmemh("instruction.txt", inst_memory); 
+// end
 assign Inst = inst_memory [Addr[ 31:2 ]];//Word Addressable 
 
 endmodule
